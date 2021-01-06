44e71c5c80aa2750db8ccda55d762f6d5b0ce1d6
Viktor Muller <viktormuller@gmail.com>
2021-01-06 22:57:03 +0000

    aDDING publish script

---
+++ publish.bat
@@ -0,0 +1,11 @@
+call build.bat %*
+
+SET TARGET_ENV=%1
+
+If "%TARGET_ENV%"=="dev" (SET S3="s3://financial-planner-3-dev"
+) ELSE If "%TARGET_ENV%"=="prod" SET S3="s3://financial-planner-3"
+
+If NOT "%TARGET_ENV%"=="local"  (
+  aws s3 rm --recursive %S3%
+  aws s3 cp --recursive build %S3%
+  )
