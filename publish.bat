09b4ac8bfa60ab6093cad22d1eb465491ff25deb
viktormuller <viktormuller@gmail.com>
2021-01-12 23:47:17 +0000

    updated for localhost building

--- publish.bat
+++ publish.bat
@@ -5,7 +5,7 @@ SET TARGET_ENV=%1
 If "%TARGET_ENV%"=="dev" (SET S3="s3://financial-planner-3-dev"
 ) ELSE If "%TARGET_ENV%"=="prod" SET S3="s3://financial-planner-3"
 
-If NOT "%TARGET_ENV%"=="local"  (
+If NOT "%TARGET_ENV%"=="localhost"  (
   aws s3 rm --recursive %S3%
   aws s3 cp --recursive build %S3%
   )
