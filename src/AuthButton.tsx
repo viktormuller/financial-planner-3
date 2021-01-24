e651c695bab85d77923317a0ed985a61257ce0b9
viktormuller <viktormuller@gmail.com>
2021-01-24 23:24:18 +0000

    improved auth

--- src/AuthButton.tsx
+++ src/AuthButton.tsx
@@ -1,4 +1,4 @@
-import { useAuth0 } from "@auth0/auth0-react";
+import { RedirectLoginOptions, useAuth0 } from "@auth0/auth0-react";
 import React from "react";
 import { Button } from "react-bootstrap";
 
@@ -7,9 +7,12 @@ export function AuthButton() {
 
   const { loginWithRedirect, logout, isAuthenticated } = useAuth0();
   let {protocol, host}  = window.location;
+  const options:RedirectLoginOptions = {
+    redirectUri: `${protocol}//${host}/`
+  }
 
   if (isAuthenticated)
     return (<Button variant="secondary" onClick={() => logout({ returnTo: `${protocol}//${host}/` })}>Logout</Button>)
-  else return <Button variant="primary" onClick={() => {loginWithRedirect();}}>Login</Button>;
+  else return <Button variant="primary" onClick={() => {loginWithRedirect(options);}}>Login</Button>;
 }
 
